5ce40938d5d05cb3c333b2d9742b578276d6bd2a
mscargill <matthewscargilluni@gmail.com>
2026-05-10 14:28:18 +0100

    early preallocated BEM matrix build

--- src/BEM/Matrix.jl
+++ src/BEM/Matrix.jl
@@ -5,10 +5,38 @@ using Base.Threads
                           xs::AbstractVector{SVector{2,Float64}},
                           ns::AbstractVector{SVector{2,Float64}},
                           w::AbstractVector{Float64},
-                          H::HankelTable;
+                          H::HankelTable,
+                          A::Matrix{ComplexF64}; # preallocated matrix to fill
                           interior::Bool = true)
     N = length(xs)
-    A = Matrix{ComplexF64}(undef, N, N)
+
+    jump = interior ? -0.5 : 0.5 # interior or exterior jump term
+
+    @threads for i in 1:N
+        xi = xs[i]
+        @inbounds begin
+            for j in 1:i-1
+                A[i,j] = kernel_bem(xi, xs[j], ns[j], k, H) * w[j]
+            end
+            A[i,i] = jump + 0im
+            for j in i+1:N
+                A[i,j] = kernel_bem(xi, xs[j], ns[j], k, H) * w[j]
+            end
+        end
+    end
+    return A
+end
+
+@inline function build_BEM_matrix(k::Float64,
+                          xs::AbstractVector{SVector{2,Float64}},
+                          ns::AbstractVector{SVector{2,Float64}},
+                          w::AbstractVector{Float64},
+                          H::HankelTable,
+                          interior::Bool = true)
+    N = length(xs)
+
+    A = Matrix{ComplexF64}(undef, N, N) # allocate memory if not provided
+
     jump = interior ? -0.5 : 0.5 # interior or exterior jump term
 
     @threads for i in 1:N
